830766d05277af778e547f34d905160928962128
Mona <mona@Monas-MacBook-Air.local>
2020-12-21 13:34:33 +0530

    Latest

--- src/app/sidebar/sidenav.service.ts
+++ src/app/sidebar/sidenav.service.ts
@@ -21,19 +21,39 @@ export class SidenavService {
     this.hideAlert = !this.hideAlert;
   }
 
+  toggleHideAlert(): void {
+    this.hideAlert = false;
+  }
+
   toggleMessage(): void {
     this.hideMessage = !this.hideMessage;
+  }
+
+  toggleHideMessage() {
+    this.hideMessage = false;
   }
 
   toggleProfile(): void {
     this.hideProfile = !this.hideProfile;
   }
 
+  toggleHideProfile(): void {
+    this.hideProfile = false;
+  }
+
   togglePage(): void {
     this.hidePage = !this.hidePage;
   }
 
+  toggleHidePage(): void {
+    this.hidePage = false;
+  }
+
   toggleQuestion(): void {
     this.hideQuestion = !this.hideQuestion;
+  }
+
+  toggleHideQuestion(): void {
+    this.hideQuestion = false;
   }
 }
