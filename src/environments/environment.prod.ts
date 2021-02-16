b1c10099bc33d057d1779c962e9b288bea480ed7
kesavnayak <kesavnayak@gmail.com>
2021-02-16 20:00:09 +0530

    Latest Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAqN9n8b5kmS7EBqTSxnInpcIQ3dDjW71o',
+    authDomain: 'techblog-75ac4.firebaseapp.com',
+    projectId: 'techblog-75ac4',
+    storageBucket: 'techblog-75ac4.appspot.com',
+    messagingSenderId: '597619215697',
+    appId: '1:597619215697:web:0222dec39a43df16391770',
+  },
 };
